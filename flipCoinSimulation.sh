e990680ad4b5300975a62ecd0b5fda8380725c24
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-03 11:40:22 +0530

    Added Double coin flip simulation functionality.

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,47 +1,51 @@
 echo "Welcome Flip Coin Simulation"
 
 read -p "Enter the number of times you want to flip the coin: " num
-
+HEAD='H'
+TAIL='T'
 declare -A coinDictionary
+declare -A coinperDict
+declare -a coinArray
+
+function getResult()
+{
+	local result=''
+	for (( i=0; i<2; i++ ))
+	do
+		coin=$(( RANDOM%2 ))
+		if [ $coin -eq 0 ]
+		then
+			result="${result}${HEAD}"
+		elif [ $coin -eq 1 ]
+		then
+			result="${result}${TAIL}"
+		fi
+	done
+	echo $result
+}
+
 
 for (( i=0; i<num; i++ ))
 do
-	coin=$(( RANDOM%2 ))
-
-	if [ $coin -eq 0 ]
+	result=$( getResult )
+	if [[ ${coinDictionary[$result]} ]]
 	then
-		if [ ${coinDictionary['H']} ]
-		then 
-			count=${coinDictionary['H']}
-			coinDictionary['H']=$(( ++count ))
-		else
-			coinDictionary['H']=1
-		fi
-	elif [ $coin -eq 1 ]
-	then
-		if [ ${coinDictionary['T']} ]
-                then
-                        count=${coinDictionary['T']}
-                        coinDictionary['T']=$(( ++count ))
-                else
-                        coinDictionary['T']=1
-		fi
+		count=${coinDictionary[$result]}
+		coinDictionary[$result]=$(( ++count ))
+	else
+		coinDictionary[$result]=1
 	fi
+done
+echo "Keys:  "${!coinDictionary[@]}
+echo "Values: "${coinDictionary[@]}
 
+# Create a array from Dictionary values
+cnt=0
+for key in ${!coinDictionary[@]}
+do
+	coinArray[$((cnt++))]=${coinDictionary[$key]}
+	per=`echo ${coinDictionary[$key]} $num | awk '{print ($1/$2)*100}'`
+	coinperDict[$key]=$per
 done
 
-echo ${!coinDictionary[@]}
-echo ${coinDictionary[@]}
-
-if [ ${coinDictionary['H']} -gt ${coinDictionary['T']} ]
-then
-	percentage=`echo ${coinDictionary['H']} $num | awk '{print ($1/$2)*100}'`
-	echo "H:"$percentage"%"
-elif [ ${coinDictionary['T']} -gt ${coinDictionary['H']} ]
-then
-        percentage=`echo ${coinDictionary['T']} $num | awk '{print ($1/$2)*100}'`
-        echo "T:"$percentage"%"
-else
-	percentage=`echo ${coinDictionary['H']} $num | awk '{print ($1/$2)*100}'`
-        echo "T:"$percentage"%"
-fi
+echo "Percent Dictonary: "${coinperDict[@]}
